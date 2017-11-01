ae2b7e0f1d2a0c8c5a7ed6fdc52572bf99bb0fa6
kalibor <sf104137@gmail.com>
2017-11-01 20:28:44 +0800

    Save1

---
+++ TestImgAreaSelect/TestImgAreaSelect/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestImgAreaSelect.Controllers
+{
+    public class ImgUploadInfo
+    {
+       public HttpPostedFileBase Image { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult ImageUpload(ImgUploadInfo imgInfo)
+        {
+            try
+            {
+                string dictPath = @"\images\temp\";
+
+                string fileName = Guid.NewGuid().ToString();
+
+                string Extension = System.IO.Path.GetExtension(imgInfo.Image.FileName);
+
+                string savePath = string.Format("{0}{1}{2}", dictPath, fileName, Extension);
+
+                var bitMap = new Bitmap(imgInfo.Image.InputStream);
+
+                bitMap.Save(Server.MapPath(savePath));
+
+                return Json(savePath);
+            }catch(Exception e)
+            {
+                var data = e.Message;
+
+                return Json(data);
+            }
+         
+        }
+    }
+}
---
+++ TestImgAreaSelect/TestImgAreaSelect/Plugin/modal/modal.js
@@ -0,0 +1,103 @@
+﻿(function ($) {
+
+    $.fn.extend({
+        setDefaultModal: function () {
+            var me = $(this);
+
+            if (me.hasClass('modal-open')) {
+                var modal = getModal(me);
+                var opt = getOption(me);
+                if (modal.length) {
+
+                    var span = modal.find('.close');
+
+                    me.on('click', function () {
+                        openModal(modal, opt.OnOpen)
+
+                    });
+
+                    span.on('click', function () {
+
+                        closeModal(modal);
+                    });
+
+                    $(window).on('click', function (event) {
+                            if (event.target == modal[0]) {
+                                closeModal(modal);
+                            }
+                      
+                    });
+
+                
+
+                }
+
+            }
+
+        },
+
+        submit: function (modal,func) {
+            var me = $(this);
+            if (me.hasClass('modal-submit')) {
+                if (func && typeof func == 'function') {
+                    func();
+                    closeModal(modal);
+                }
+            } 
+        },
+
+        getModal: function () {
+            var me = $(this);
+
+            if (me.hasClass('modal-open')) {
+                return getModal(me);
+            } else {
+                return null;
+            }
+
+        },
+        getOption: function () {
+            var me = $(this);
+            if (me.hasClass('modal-open')) {
+                return getOption(me);
+            } else {
+                return null;
+            }
+        }
+
+    });
+
+
+    var getModal = function (btn) {
+        var targetModalId = btn.data('target');
+
+        return $(document.getElementById(targetModalId));
+
+    };
+
+
+    var getOption = function (btn) {
+        var opt = btn.data('option');
+        return opt;
+
+    };
+
+    var closeModal = function (modal) {
+      
+        var modalContent = modal.find('.modal-body');
+        modalContent.empty();
+        modal.css('display', "none");
+    
+    };
+
+    var openModal = function (modal,func) {
+        modal.css('display', "block");
+        if (func && typeof func == 'function') {
+            func();
+        }
+
+    }
+
+})(jQuery);
+
+
